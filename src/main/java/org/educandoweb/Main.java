79f4c7e5182c6d05c5aef3fa7a19f142c4fafc02
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2025-02-02 22:38:10 -0300

    First Class: Position

--- src/main/java/org/educandoweb/Main.java
+++ src/main/java/org/educandoweb/Main.java
@@ -1,11 +1,14 @@
 package org.educandoweb;
 
+import boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
 
     }
 }
